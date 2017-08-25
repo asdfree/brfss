2e93a60055ac0c05854c613428615eb15e5fb7b0
Anthony Damico <ajdamico@gmail.com>
2017-08-25 02:32:39 -0400

    brfss 100% maybe

--- tests/setup.R
+++ tests/setup.R
@@ -1,17 +1,4 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
 library(lodown)
-
-brfss_cat <-
-	get_catalog( "brfss" ,
-		output_dir = file.path( getwd() ) )
-
-# sample 75% of the records
-which_records <- sample( seq( nrow( brfss_cat ) ) , round( nrow( brfss_cat ) * 0.75 ) )
-
-# always sample year == 2015
-brfss_cat <- unique( rbind( brfss_cat[ which_records , ] , subset( brfss_cat , year == 2015 ) ) )
-
-brfss_cat <- brfss_cat[ rev( order( brfss_cat$year ) ) , ]
-
-lodown( "brfss" , brfss_cat )
+lodown( "brfss" , output_dir = file.path( getwd() ) )
